41bc42467cbc3095f0f3d0dc3733ea1c06fad931
Andrew2020WIT <andrew2020wit@gmail.com>
2020-09-15 14:00:31 +0300

    ArticlesList, Apollo, gql

---
+++ src/app/articles/article-header-card/article-header-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-header-card',
+  templateUrl: './article-header-card.component.html',
+  styleUrls: ['./article-header-card.component.scss'],
+})
+export class ArticleHeaderCardComponent implements OnInit {
+  @Input() title = 'no title';
+  @Input() authorFullName = 'no author';
+  @Input() description = 'no description';
+  @Input() createdOn: Date;
+  @Input() updatedOn: Date;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ArticlesModule } from './../articles/articles.module';
 import { ShareModule } from './../share.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [HomePageComponent, NotFoundPageComponent],
-  imports: [CommonModule, ShareModule, RouterModule],
+  imports: [CommonModule, ShareModule, RouterModule, ArticlesModule],
 })
 export class ViewModule {}
