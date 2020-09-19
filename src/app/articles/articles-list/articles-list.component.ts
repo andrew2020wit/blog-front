7e6d76b309a236432b5dda95811c29b2f96fc5f9
Andrew2020WIT <andrew2020wit@gmail.com>
2020-09-19 15:25:12 +0300

    ArticlesList: infinite scroll,IntersectionObserver

--- src/app/articles/articles-list/articles-list.component.ts
+++ src/app/articles/articles-list/articles-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Apollo, gql } from 'apollo-angular';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Apollo, gql, QueryRef } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 import { IArticleHeader } from './../article-header.interface';
 
 const ArticlesGQL = gql`
-  query {
-    allArticles {
+  query allArticles($take: Int, $createOnCursor: DateTime) {
+    allArticles(take: $take, createOnCursor: $createOnCursor) {
       id
       title
       description
@@ -24,27 +24,78 @@ const ArticlesGQL = gql`
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.scss'],
 })
-export class ArticlesListComponent implements OnInit, OnDestroy {
-  private querySubscription: Subscription;
-  // MatPaginator Inputs
-  length = 100;
-  pageSize = 10;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-  loading: boolean;
+export class ArticlesListComponent implements OnInit, AfterViewInit, OnDestroy {
+  // intersection
+  private intersectionObserver: IntersectionObserver;
+  isIntersecting2 = false;
   articleHeaders: IArticleHeader[] = [];
-  constructor(private apollo: Apollo) {}
+
+  private querySubscription: Subscription;
+  ArticlesWatchQuery: QueryRef<any>;
+  takeV = 20;
+  createOnCursor: Date = new Date();
+  dataFinished = false;
+  loading = false;
+
+  constructor(private apollo: Apollo) {
+    this.intersectionObserver = new IntersectionObserver((entries) =>
+      this.intersectionCallback(entries)
+    );
+  }
 
   ngOnInit(): void {
-    this.querySubscription = this.apollo
-      .watchQuery<any>({
-        query: ArticlesGQL,
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        this.loading = loading;
-        this.articleHeaders = data.allArticles;
-      });
+    this.ArticlesWatchQuery = this.apollo.watchQuery<any>({
+      query: ArticlesGQL,
+      variables: {
+        take: this.takeV,
+        createOnCursor: this.createOnCursor,
+      },
+    });
   }
+  ngAfterViewInit(): void {
+    this.intersectionObserver.observe(
+      document.getElementById('IntersectionTarget')
+    );
+    this.querySubscription = this.ArticlesWatchQuery.valueChanges.subscribe(
+      ({ data, loading }) => {
+        this.loadArtHeaders({ data, loading });
+      }
+    );
+  }
+
+  intersectionCallback(entries) {
+    this.isIntersecting2 = entries[0].isIntersecting;
+    if (this.isIntersecting2) {
+      this.loadArtHeadersNext();
+    }
+  }
+
+  loadArtHeaders({ data, loading }) {
+    this.loading = loading;
+    const newArticles: IArticleHeader[] = data.allArticles as IArticleHeader[];
+    const length = newArticles.length;
+    if (length < this.takeV) {
+      this.dataFinished = true;
+    }
+    if (length > 0) {
+      this.createOnCursor = newArticles[length - 1].createdOn;
+    }
+    this.articleHeaders.push(...newArticles);
+  }
+
+  loadArtHeadersNext() {
+    if (this.dataFinished) {
+      return;
+    }
+    this.ArticlesWatchQuery.fetchMore({
+      variables: { createOnCursor: this.createOnCursor },
+    }).then(({ data, loading }) => {
+      this.loadArtHeaders({ data, loading });
+    });
+  }
+
   ngOnDestroy() {
     this.querySubscription.unsubscribe();
+    this.intersectionObserver.disconnect();
   }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,9 +55,9 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+/** IntersectionObserver polyfill for IE and Edge */
+import 'intersection-observer';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
